1c3427f92458a05d6eb258feb2c83452de42c2d9
dev-inovacode <devjr.inovacode@gmail.com>
2021-05-07 20:25:38 -0300

    07/05/2021

---
+++ pages/api/dash/[type]/index.js
@@ -0,0 +1,43 @@
+import dbConnect from '../../dbConnect'
+import Question from '../../models/QuestionsSchemas'
+
+export default async (request, response) => {
+    dbConnect()
+
+    const {type} = request.query
+
+    switch (type) {
+        case 'weekResume':
+            const items = await Question.find({ "type": { $ne: "SUGESTAO" } })
+
+            let dt = new Date()
+            dt.setHours(dt.getHours() - 3)
+            let dt_reg = ("0" + dt.getDate()).slice(-2) + "/" + ("0" + (dt.getMonth() + 1)).slice(-2) + "/" + dt.getFullYear()
+            //para para deploy na vercel atraso de 3 horas
+
+            let weekResume = []
+
+            for(let i = 0; i < 15; i++) {
+                let dt = new Date()
+                dt.setHours(dt.getHours() - 3)
+                dt.setDate(dt.getDate() - i)
+                let dt_reg = ("0" + dt.getDate()).slice(-2) + "/" + ("0" + (dt.getMonth() + 1)).slice(-2) + "/" + dt.getFullYear()
+
+                const qtd = items.filter((item) => {
+                    if(item.date.search(dt_reg) > -1){return item}
+                }).length
+
+                weekResume.push({date: dt, value: qtd})
+            }
+
+            response.json(weekResume)
+            break
+
+        case value:
+            break
+            
+        default:
+            return response.status(404)
+            break
+        }
+}
--- pages/dashboards.js
+++ pages/dashboards.js
@@ -2,8 +2,19 @@ import Head from 'next/head'
 import Menu from './components/Menu'
 import Dashboard from './components/Dashboard'
 import ChartLine from './components/ChartLine'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function Dashboards() {
+    const [week, setWeek] = useState([])
+    useEffect(() => {
+        axios.get('/api/dash/weekResume').then(
+            response => {
+                setWeek(response.data)
+            }
+        )
+    }, [])
+
     return(
         <div className="mainContainer">
             <Head>
@@ -17,7 +28,7 @@ function Dashboards() {
                     </div>
                     <div style={{marginTop: '20px'}}>
                         <Dashboard data={[12, 36, 55, 25, 35, 10, 40]} width={500} height={400} color={'blue'}/>
-                        <ChartLine data={[12, 36, 55, 25, 35, 10, 40]}  width={500} height={400} color={'blue'}/>
+                        <ChartLine dataa={week}  width={500} height={400}/>
                     </div>
                 </div>
             </div>
